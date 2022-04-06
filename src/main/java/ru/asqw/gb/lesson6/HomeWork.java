ee6b4a149224a57fd53433e1d25dc6afe6be9296
Firewind451 <eagle_one@mail.ru>
2022-04-06 23:35:46 +0300

    ДЗ к уроку #6

---
+++ src/main/java/ru/asqw/gb/lesson6/HomeWork.java
@@ -0,0 +1,32 @@
+package ru.asqw.gb.lesson6;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        int totalCats = 0; // без явной инициализации IDEA ругается ошибкой
+        int totalDogs = 0; // без явной инициализации IDEA ругается ошибкой
+        int totalAnimals = 0; // без явной инициализации IDEA ругается ошибкой
+        Animals[] animals = {
+                new Animals("Animal1"),
+                new Animals("Animal2"),
+                new Cat("Barsik"),
+                new Dog("Bobik")
+        };
+
+        for (int i = 0; i < animals.length; i++) {
+            animals[i].run(50);
+            animals[i].swim(15);
+            System.out.println();
+            if(animals[i] instanceof Cat){
+                totalCats++;
+            } else if (animals[i] instanceof Dog){
+                totalDogs++;
+            } else if (animals[i] instanceof Animals){
+                totalAnimals++;
+            }
+        }
+
+        System.out.println("Total animals: " + totalAnimals);
+        System.out.println("Total cats: " + totalCats);
+        System.out.println("Total dogs: " + totalDogs);
+    }
+}
